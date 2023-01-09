4f391cf45f0a79742cbadf50810b2b854242e005
Bryan Malyn <bim9262@gmail.com>
2023-01-09 00:48:02 -0600

    Clean up private-keys-v1.d

--- bin/executable_yubikey-gpg-refresh.sh
+++ bin/executable_yubikey-gpg-refresh.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p $GNUPGHOME/private-keys-v1.d.old
+[ "$(ls -A $GNUPGHOME/private-keys-v1.d)" ] &&  mv $GNUPGHOME/private-keys-v1.d/* $GNUPGHOME/private-keys-v1.d.old
 gpg --card-status
 gpgconf --kill gpg-agent
 gpgconf --launch gpg-agent
